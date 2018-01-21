afdd9a0248352b0442640fa2bfa1bdc48f545428
Manoj Nair <Manoj.Nair@outlook.com>
2018-01-22 10:32:06 +1100

    Initial Commit

---
+++ HelloWorld/Services/IGreeter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace HelloWorld.Services
+{
+    public interface IGreeter
+    {
+
+        string GetMessage();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private readonly IConfiguration _configuration;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GetMessage()
+        {
+            return _configuration["Greeting"];
+            
+        }
+    }
+}
